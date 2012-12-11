9a758b10c57b1a5f844844bd346f55c48afa59d3
Alexander Presber <post@momolog.info>
2012-12-12 00:21:44 +0100

    more out

--- lib/nanoc/base/source_data/site.rb
+++ lib/nanoc/base/source_data/site.rb
@@ -346,6 +346,7 @@ module Nanoc
 
         # Read config from config.yaml in given dir
         config_path = File.join(dir_or_config_hash, 'config.yaml')
+        puts ">>> reading config.yaml and symboliting keys"
         @config = DEFAULT_CONFIG.merge(YAML.load_file(config_path).symbolize_keys)
         @config[:data_sources].map! { |ds| ds.symbolize_keys }
       else
